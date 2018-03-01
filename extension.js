b5010d1f13397a924426c2e93627cd29d3197b3a
Florian Reimair <florian.reimair@itdevas.at>
2018-03-01 13:20:22 +0100

    cleanup

--- extension.js
+++ extension.js
@@ -2,34 +2,6 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Meta = imports.gi.Meta;
-const Tweener = imports.ui.tweener;
-
-let text;
-
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
-
-function _showHello(label) {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: label });
-        Main.uiGroup.add_actor(text);
-    }
-
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(monitor.x + Math.floor(monitor.width / 3 - text.width / 2),
-                      monitor.y + Math.floor(monitor.height / 3 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
-}
 
 function init() {
 
